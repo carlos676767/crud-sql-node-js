32608fb8134ac6566402dc571bcc32a18dc4ede8
carlos676767 <carlitobr730@gmail.com>
2024-07-29 20:36:57 -0700

    Remove node_modules from repository

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const api = express()
+api.get("/", (req, res) => {
+    res.send("ola mundo")
+})
+
+
+
+
+
+
+
+const port = 8080
+api.listen(port, () => {
+    console.log("server running on port 8080");
+})
