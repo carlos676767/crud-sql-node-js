4ad185d6e66fabf11b19ba253027a4a666cd4ca1
carlos676767 <carlitobr730@gmail.com>
2024-07-30 00:22:09 -0700

    algumas validacoes ok

--- frontend/app.js
+++ frontend/app.js
@@ -7,17 +7,43 @@ class reqHttp {
   async cadastrarUse() {
     try {
       const httpReq = await axios.post(this.url, {
-        name: "carlos",
+        name: "joao",
         idade: 20,
         sexo: "m",
         nascimento: "04/05/24",
-        cpf: "32393566007"
+        cpf: "3239354602",
       });
       console.log(httpReq.data);
     } catch (error) {
       console.log(error);
     }
   }
+
+  async deleteUsers() {
+    try {
+      const httpDelete = await axios.delete(this.url)
+      console.log(httpDelete.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getPersons(){
+    try {
+      const httpGet = await axios.get(this.url)
+      console.log(httpGet);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  async updateDateOfBirth(){
+
+  }
 }
 
-new reqHttp("http://localhost:8080/cadastrarUser").cadastrarUse()
+// new reqHttp("http://localhost:8080/cadastrarUser").cadastrarUse()
+// new reqHttp("http://localhost:8080/deletePersons").deleteUsers()
+new reqHttp("http://localhost:8080/getPerson").getPersons()
+
